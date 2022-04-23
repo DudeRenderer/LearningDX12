19b696ac8c790894c0783ee159d1b0f523ddbbc8
emersonjiang <jiangheao@outlook.com>
2022-04-23 22:01:11 +0800

    fix undefined IID_ID3D12Device symbol

--- Common/SampleBase/SampleBase.h
+++ Common/SampleBase/SampleBase.h
@@ -13,9 +13,12 @@
 #include "GameTimer.h"
 
 // Link necessary d3d12 libraries.
-#pragma comment( lib, "d3dcompiler.lib" )
-#pragma comment( lib, "D3D12.lib" )
-#pragma comment( lib, "dxgi.lib" )
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+
+
 
 class SampleBase
 {
